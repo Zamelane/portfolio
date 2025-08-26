243d66097efa919ac091b44bbbae49435a327c32
Zamelane <zamelane@vk.com>
2025-08-26 18:54:02 +0700

    Статика

--- src/components/noSSRProviders.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import dynamic from "next/dynamic";
-
-export const NoSSRProviders = dynamic(
-  () => import('./providers').then(_ => _.Providers),
-  { ssr: false }
-)
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export type ChildrenProps<T = React.ReactNode> = Readonly<{
-  children: T;
-}>
-
-export type ChildrenOptionalProps<T = React.ReactNode> = Readonly<{
-  children?: T;
-}>
