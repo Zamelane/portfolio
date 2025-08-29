50b4113cbe5082d81529b7b540046f3bd3359bb7
Zamelane <zamelane@vk.com>
2025-08-29 17:28:49 +0700

    feat: preanimtions

--- app/(app)/projects/page.tsx
+++ app/(app)/projects/page.tsx
@@ -5,6 +5,7 @@ import { ProjectCard } from "@/src/components/projects/card";
 import { SelectItem } from "@/src/components/selectItem";
 import { Spoiler } from "@/src/components/spoiler";
 import { projects, projectTechnologies } from "@/static/projects/content";
+import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 export default function Page() {
@@ -54,21 +55,23 @@ export default function Page() {
             </p>
           </Tab>
         </TabLayout>
-        <div className="h-full w-full md:overflow-y-auto grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] sm:grid-cols-[repeat(auto-fill,minmax(350px,1fr))] gap-7 p-16 not-md:p-6">
-          {
-            projects.map((project, i) => {
-              // Проверяем, что подходят ВСЕ фильтры
-              const isIncluded = selected.length === 0 || !selected.some(item => !project.meta.technologies.includes(item))
-              
-              if (!isIncluded) {
-                return undefined
-              }
-              
-              // TODO: Закостылил паддинг. Почеу-то не получилось его по-другому добавить
-              return <ProjectCard key={i} data={project} className={i === projects.length - 1 && 'pb-10' || undefined} />
-            })
-          }
-        </div>
+        <AnimatePresence mode="popLayout">
+          <div className="h-full w-full md:overflow-y-auto grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] sm:grid-cols-[repeat(auto-fill,minmax(350px,1fr))] gap-7 p-16 not-md:p-6">
+            {
+              projects.map((project, i) => {
+                // Проверяем, что подходят ВСЕ фильтры
+                const isIncluded = selected.length === 0 || !selected.some(item => !project.meta.technologies.includes(item))
+
+                if (!isIncluded) {
+                  return undefined
+                }
+
+                // TODO: Закостылил паддинг. Почеу-то не получилось его по-другому добавить
+                return <ProjectCard key={i} data={project} className={i === projects.length - 1 && 'pb-10' || undefined} />
+              })
+            }
+          </div>
+        </AnimatePresence>
       </div>
 
     </div>
--- src/components/projects/card.tsx
+++ src/components/projects/card.tsx
@@ -15,20 +15,20 @@ export function ProjectCard({ data, className }: ProjectCardType) {
     ? data.image
     : `/static/${data.slug}/${data.image}`
   return (
-    <div className={cn("flex flex-col gap-4 w-full", className)}>
+    <div className={cn("flex flex-col gap-4 w-full select-none", className)}>
 
       <p className="whitespace-pre pl-4">
         <span className="text-indigo-500 font-bold">Project {projects.indexOf(data) + 1}</span>
         <span>{" // " + data.slug}</span>
       </p>
 
-      <div className="min-h-80 bg-slate-950 border border-slate-800 rounded-2xl overflow-clip">
-        <div className="min-h-56 max-h-56 overflow-hidden relative">
-          <Image src={imgSrc} alt="" fill className="object-cover" />
+      <div className="group hover:border-slate-300 hover:scale-[99%] duration-300 hover:shadow-md shadow-slate-900 transition-all min-h-80 bg-slate-950 border border-slate-800 rounded-2xl overflow-clip">
+        <div className="min-h-56 max-h-56 overflow-clip relative">
+          <Image src={imgSrc} alt="" fill className="object-cover transition-transform duration-300 group-hover:scale-110" />
           <i></i>
         </div>
 
-        <div className="border-t border-t-slate-800 p-8 flex flex-col gap-5">
+        <div className="border-t border-t-slate-800 group-hover:border-t-slate-300 p-8 flex flex-col gap-5">
           <p>{ data.title }</p>
           <Button className="bg-slate-600 text-white max-w-fit">view-project</Button>
         </div>
